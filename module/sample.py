537ef4997f98d7a196907eafb98f0ef7d65746d8
SeiichiN <billie175@gmail.com>
2023-11-13 16:33:01 +0900

    module added.

---
+++ module/sample.py
@@ -0,0 +1,5 @@
+from some_func import get_circle_area
+
+r = 20
+area = get_circle_area(r)
+print(area)
---
+++ module/some_func.py
@@ -0,0 +1,10 @@
+import math
+
+def get_triangle_area(base, height):
+    return base * height / 2
+
+def get_rectangle_area(width, height):
+    return width * height
+
+def get_circle_area(radius):
+    return radius ** 2 * math.pi
